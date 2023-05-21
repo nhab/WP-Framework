013573e0c66c45430bc23f33d453ae9e14df452e
Nima Habibollahi <n.habibollahi@yahoo.com>
2023-05-21 18:27:20 +0330

    Update and rename add_init_action.php to action_init_add.php

--- Actions/action_init_add.php
+++ Actions/action_init_add.php
@@ -1,32 +1,29 @@
 <?php
 /**
-plugin name: add_init_action
 description: it adds an init action on top previousely added init actions, without overwriting them.
 */
-
 function add_my_init_method($id, $method_name) {
    add_action('init', function() use ($id, $method_name) {
       call_user_func($method_name, $id);
    });
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-
-Summary: it adds an init action on top previousely added init actions, without overwriting them.   
+/**
+description: it adds an init action on top previousely added init actions, without overwriting them.   
 
 Examples:
 
 function crappy_method(){
-	update_option('sitemsg',get_option('site_msg').'c');
+	update_option('sitemsg',get_option('site_msg').'1');
 }
 function even_crappier_method(){
-	update_option('sitemsg',get_option('site_msg').'C');
+	update_option('sitemsg',get_option('site_msg').'2');
 }
 
-add_init_method('crappy_method' );
-add_init_method( 'even_crappier_method' );
+action_init_add('crappy_method' );
+action_init_add( 'even_crappier_method' );
 
 *////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function add_init_action($method_name) {
- add_my_init_method($method_name,$method_name);
+function action_init_add($method_name) {
+ 	add_my_init_method($method_name,$method_name);
 }
