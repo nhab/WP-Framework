14a452c1dac7794905ba75c932bb2d1397aa07c2
Nima Habibollahi <n.habibollahi@yahoo.com>
2023-05-21 18:55:22 +0330

    Update option_helper.php

--- option_helper.php
+++ option_helper.php
@@ -1,16 +1,20 @@
-if(!function_exists('option_msg')){
-  function option_msg($msg){
-    $oldermsgs=get_option('site_msg');
-    if($oldermsgs!=null)
-      update_option('site_msg', $oldermsgs.$msg);
-    ele
-      add_option('site_msg', $msg);
+<?php
+
+if (!function_exists('option_exists')){
+	function option_exists( $option_name, $site_wide = false ) {
+      global $wpdb; 
+	    $prefix= ($site_wide ? $wpdb->base_prefix : $wpdb->prefix);
+			$query_rest="options WHERE option_name ='%s' LIMIT 1" ;
+			$r= $wpdb->query( $wpdb->prepare( "SELECT * FROM ".$prefix.$query_rest, $option_name));
+		
+			
+			return $r;
   }
 }
 
-if(!function_exists('option_exists')){
-  function option_exists( $option_name, $site_wide = false ) {
-    global $wpdb; 
-    return $wpdb->query( $wpdb->prepare( "SELECT * FROM ". ($site_wide ? $wpdb->base_prefix : $wpdb->prefix). "options WHERE option_name ='%s' LIMIT 1", $option_name ) );
-  }
+if (!function_exists('option_msg')){
+	function option_msg($msg){
+		//$msg=$msg.get_option('site_msg');
+		update_option('site_msg', get_option('site_msg'));
+	}
 }
